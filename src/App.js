a687be548d249f39af4598a7fe7a6d810eac9a74
jaugs <johnaugsburger@gmail.com>
2022-12-04 16:56:27 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function changeTab() {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>Hello from App</p>
+        <button onClick={changeTab}>Click Me To switch</button>
       </header>
+
+      <div id="sidebar">
+        <h1>React Router Contacts</h1>
+        <div>
+          <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search Products"
+              placeholder="Search Products"
+              type="search"
+              name="q"
+            />
+            <div
+              id="search-spinner"
+              aria-hidden
+              hidden={true}
+            />
+            <div
+              className="sr-only"
+              aria-live="polite"
+            ></div>
+          </form>
+          <form method="post">
+            <button type="submit">New</button>
+          </form>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href={`cart`}>Your Name</a>
+            </li>
+            <li>
+              <a href={`contacts/2`}>Your Friend</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="detail"></div>
+  
+
     </div>
   );
 }
---
+++ src/components/cart.js
@@ -0,0 +1,12 @@
+
+
+const Cart = () => {
+    return (
+      <div>
+        <h1>Hello from Cart</h1>
+        <a href={`/`}>Your Name</a>
+      </div>
+    );
+  };
+  
+  export default Cart;
